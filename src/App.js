831b80e80323d84533ed9bf4ae3b29dde333431f
Ausaf Abbas <abbas@ABBAS>
2023-07-14 21:16:06 +0530

    react-router-dom added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import About from './components/About';
 function App() {
   return (
     <>
-    
+    <BrowserRouter>
       <Navbar title="TextUtils"/>
-  <div className="container"> <TextForm heading="Enter your text to analyze below" />
-</div>
+      <div className="container">
+      <Routes>
+       <Route path="/" element={<TextForm heading="Enter your text to analyze below" />}></Route>
+      <Route path="/about" element={<About/>}></Route>
+</Routes>
+</div>/</BrowserRouter>
     </>
   );
 }
